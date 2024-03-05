c0c72e720211c3cea8a79123a8c92fb640fe4ff1
amirhosein <amir.nero1@gmail.com>
2024-03-05 19:40:55 +0330

    Init Project

---
+++ app/src/main/java/com/photo/mahsa/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.photo.mahsa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.photo.mahsa.ui.theme.MahsaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MahsaTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
