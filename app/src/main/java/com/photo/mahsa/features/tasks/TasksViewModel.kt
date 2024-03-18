27ae4a7cb8fb1183c34133c91eed06248afb1ff8
amir <amir.nero1@gmail.com>
2024-03-18 10:35:23 +0330

    Update Kotlin version

--- app/src/main/java/com/photo/mahsa/features/tasks/TasksViewModel.kt
+++ app/src/main/java/com/photo/mahsa/features/tasks/TasksViewModel.kt
@@ -1,21 +1,25 @@
 package com.photo.mahsa.features.tasks
 
 import androidx.lifecycle.ViewModel
-import com.photo.mahsa.ui.model.TaskState
+import com.photo.mahsa.ui.model.Task
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(
 
 ): ViewModel() {
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
 
 }
 
 data class UiState(
     val loading: Boolean = false,
     val errorMessage: String? = null,
-    val tasks: ImmutableList<TaskState> = persistentListOf()
+    val tasks: ImmutableList<Task> = persistentListOf()
 )
