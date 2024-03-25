fe07c3a281103fdc85827544e6f84b8407193e7a
amirhosein <amir.nero1@gmail.com>
2024-03-25 11:53:00 +0330

    Add modal navigation drawer

--- app/src/main/java/com/photo/mahsa/MainActivity.kt
+++ app/src/main/java/com/photo/mahsa/MainActivity.kt
@@ -3,19 +3,26 @@ package com.photo.mahsa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.twotone.AddCircle
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -25,6 +32,7 @@ import androidx.navigation.compose.rememberNavController
 import com.photo.mahsa.navigation.AppNavHost
 import com.photo.mahsa.ui.theme.MahsaTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -34,42 +42,65 @@ class MainActivity : ComponentActivity() {
         setContent {
             MahsaTheme {
                 val navController = rememberNavController()
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
 
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        TopAppBar(
-                            modifier = Modifier.shadow(
-                                elevation = 24.dp,
-                                spotColor = Color.DarkGray
-                            ),
-                            title = {
-                            Text(text = stringResource(id = R.string.app_name))
-                        }, navigationIcon = {
-                            Icon(imageVector = Icons.Default.Menu, contentDescription = null)
-                        })
-                    }, floatingActionButton = {
-                        FloatingActionButton(onClick = {
-                            // navigate to add task
-                        }) {
-                            Icon(
-                                imageVector = Icons.TwoTone.AddCircle,
-                                contentDescription = null
+                ModalNavigationDrawer(
+                    drawerContent = {
+                        ModalDrawerSheet(
+                            drawerTonalElevation = 32.dp,
+                            drawerShape = RoundedCornerShape(0.dp)
+                        ) {
+                            Text(text = "Content")
+                        }
+                    },
+                    drawerState = drawerState
+                ) {
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        topBar = {
+                            TopAppBar(
+                                modifier = Modifier.shadow(
+                                    elevation = 8.dp,
+                                    spotColor = Color.DarkGray
+                                ),
+                                title = {
+                                    Text(text = stringResource(id = R.string.app_name))
+                                }, navigationIcon = {
+                                    Icon(
+                                        imageVector = Icons.Default.Menu,
+                                        contentDescription = null,
+                                        modifier = Modifier.clickable {
+                                            scope.launch {
+                                                if (drawerState.isClosed) drawerState.open()
+                                            }
+                                        })
+                                })
+                        }, floatingActionButton = {
+                            FloatingActionButton(onClick = {
+                                // navigate to add task
+                            }) {
+                                Icon(
+                                    imageVector = Icons.TwoTone.AddCircle,
+                                    contentDescription = null
+                                )
+                            }
+                        }) { innerPadding ->
+                        Surface(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            AppNavHost(
+                                modifier = Modifier.fillMaxSize(),
+                                navController = navController
                             )
                         }
-                    }) { innerPadding ->
-                    Surface(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                    ) {
-                        AppNavHost(modifier = Modifier.fillMaxSize(), navController = navController)
                     }
                 }
             }
         }
     }
 }
-
 
